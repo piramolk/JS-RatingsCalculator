19c645a70dd6bc1f128bb208a064f586c6fea090
Piramol Krishnan <piramolkk@gmail.com>
2022-06-27 23:51:00 +0800

    Initial test

---
+++ js/ratings.js
@@ -0,0 +1,21 @@
+function Rating(){
+    this.count=0;
+    this.sum=0;
+    this.average=0;
+}
+
+
+let collect_ratings = function(){
+    let ratings = Rating();
+    let rating = 0;
+    const elements = document.querySelectorAll(type="ratings");
+    
+    elements.forEach(function(element){
+        rating = parseInt(element.id.replace("star",""));
+        ratings.count += parseInt(element.value);
+        ratings.sum += parseInt(element.value)*rating;
+    })
+    if(ratings.count!=0){
+        ratings.average = sum/count;
+    }
+}
